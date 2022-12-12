e003f339f90243da1c6087827f9a56fa43172b21
Ankitsingh Bisht <ankitsingh.cs18018@mmcc.edu.in>
2022-12-12 17:35:24 +0530

    Delete functionality added with design changes

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ function loadUser() {
       <tr>`;
 
       for (var i in users) {
-        table.innerHTML +=
-          "<tr id='tr'> <td id='td'>" +
-          users[i].fName +
-          "</td><td id='td'>" +
-          users[i].lName +
-          "</td><td id='td'>" +
-          users[i].age +
-          "</td><td id='td'> <button id='edit'>Edit</button>&nbsp<button id='delete'>Delete</button>";
+        table.innerHTML += `
+        <tr>
+            <td>${users[i].fName}</td>
+            <td>${users[i].lName}</td>
+            <td>${users[i].age}</td>
+            <td><button id="edit" onClick='editData("${users[i].id}")'>Edit</button></td>&nbsp
+            <td><button id="delete" onclick='deleteData("${users[i].id}")'>Delete</button></td>&nbsp
+        </tr>`;
       }
     }
   };
@@ -67,15 +67,13 @@ getDataByID.addEventListener("submit", (e) => {
       console.log(user);
       var table = document.getElementById("tableGDBI");
       let data = `<table id="getByIDTable">
-      <tr>
-       <th>Name</th>
-       <th>Last Name</th>
-       <th>Age</th>
-      </tr>
+      
       <tr>
        <td>${user.fName}</td>
-       <td>${user.lName}</td>\
+       <td>${user.lName}</td>
        <td>${user.age}</td>
+      <td><button id="edit" onclick='editData("${user.id}")'>Edit</button></td>
+      <td><button id="delete" onclick='deleteData("${user.id}")'>Delete</button></td>
       </tr>
       
      </table>`;
@@ -83,4 +81,46 @@ getDataByID.addEventListener("submit", (e) => {
     }
   };
   xhr.send();
+  document.getElementById("getDataByID").reset();
 });
+
+//Deleting a record
+function deleteData(testId) {
+  console.log(testId);
+  var xhr = new XMLHttpRequest();
+
+  xhr.open("DELETE", `http://127.0.0.1:3000/users/${testId}`, true);
+  xhr.onload = function () {
+    if (this.status == 200) {
+      alert(`Successfully deleted ${testId}`);
+      location.reload();
+    }
+  };
+  xhr.send();
+}
+
+function editData(updateId) {
+  console.log(updateId);
+}
+//Updating the data
+
+// const Updateform = document.getElementById("UpdateDataById");
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   const params = {
+//     fName: document.querySelector("#fNameU").value,
+//     lName: document.querySelector("#lNameU").value,
+//     age: document.querySelector("#ageU").value,
+//   };
+
+//   const http = new XMLHttpRequest();
+//   http.open("PATCH", "http://127.0.0.1:3000/users/");
+//   http.setRequestHeader("Content-type", "application/json");
+//   http.send(JSON.stringify(params));
+//   http.onload = function () {
+//     alert(http.responseText);
+//   };
+//   document.getElementById("postData").reset();
+// });
